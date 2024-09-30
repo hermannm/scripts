85cc8532e3277d9b128f477ed8178ca92efa6b25
hermannm <hermann.morkrid@gmail.com>
2024-09-30 10:41:25 +0200

    Set with_target to false on devlog-tracing subscriber

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use tracing::error;
 #[inline]
 #[must_use] // must_use, so ExitCode is not accidentally discarded
 pub fn run_script(script: impl FnOnce() -> anyhow::Result<()>) -> ExitCode {
-    devlog_tracing::subscriber().init();
+    devlog_tracing::subscriber().with_target(false).init();
 
     let result = script();
     match result {
